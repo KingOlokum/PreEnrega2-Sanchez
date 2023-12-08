55f5da69c22c4713cf11b62f656d813a423d633d
tienda_marroquinera <andressanchez03@gmail.com>
2023-12-08 13:21:12 -0500

    seleccion de categorias

--- src/App.js
+++ src/App.js
@@ -39,7 +39,9 @@ function App() {
       <Route path="/" element={<ItemListContainer />} />
       <Route path="/category/:id" element={<ItemListContainer />} />
       <Route path="/item/:id" element={<ItemDetailContainer />} />
+      <Route path='/category/:id/item/:id' element={<ItemDetailContainer/>}/>
       <Route path="/cart" element={<Cart />} />
+
       
     </Routes>
     </CartProvider>
